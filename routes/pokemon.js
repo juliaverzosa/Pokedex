755923c76b8c4031b5b1548c6a4b4a4d450b2c93
juliaverzosa <j.verzosa.534381@umindanao.edu.ph>
2025-06-05 16:54:26 +0800

    basic Pokédex functionality with home, search, and details  pages

---
+++ routes/pokemon.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import axios from 'axios';
+import env from 'dotenv';
+
+
+env.config();
+
+const router = express.Router();
+const API_URL = process.env.API_URL;
+
+
+router.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(API_URL); 
+        res.render("pages/home", { pokemonList: response.data.results});
+    } catch (error) {
+        res.status(500).send("Error fetching Pokemon data");
+    }
+})
+
+router.get("/pokemon/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        res.render("pages/details", { pokemon: response.data });
+    } catch (error) {
+        res.status(500).send("Error fetching Pokémon details");
+    }
+})
+
+router.get("search", async (req, res) => {
+    try {
+        const { name } = req.query;
+        if (!name) return res.redirect("/");
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        res.render("pages/details", { pokemon: response.data });
+    } catch (error) {
+        res.status(500).send("Error fetching Pokémon data");
+    }
+})
+
+router.get("/search", async (req, res) => {
+    try {
+        const { name } = req.query;
+        if (!name) return res.redirect("/");
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        res.render("pages/details", { pokemon: response.data });
+    } catch (error) {
+        res.status(500).send("Error fetching Pokémon data");
+    }
+});
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import pokemonRoutes from '../routes/pokemon.js'
+
+const app = express();
+const PORT  = 3000;
+
+const __filename =  fileURLToPath(import.meta.url);
+
+app.set("view engine", "ejs");
+
+app.use("/", pokemonRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
